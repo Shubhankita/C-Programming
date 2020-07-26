f9df2fa62662269948ec652394b8fc21feff3db4
Coursera Learner <nobody@nowhere.nul>
2020-07-26 12:11:05 +0000

    Max defined

--- 14_array_max/arrayMax.c
+++ 14_array_max/arrayMax.c
@@ -4,7 +4,7 @@
 int * arrayMax(int * array, int n) {
   if (array != NULL && n!=0){
     int large = 0;
-    for(int i =0; i<n ; i++)
+    for(int i =1; i<n ; i++)
       {
 	if( array[i]> array[large])
 	  large = i;
